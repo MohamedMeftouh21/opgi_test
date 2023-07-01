d64f8674877316452167a99d3a5ff43bc75f7e2e
MohamedMeftouh21 <moaddameftouh@gmail.com>
2023-07-02 00:04:48 +0100

    first commit

--- opgi/settings.py
+++ opgi/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'import_export',
      'data',
     'chat.apps.ChatConfig',
  'accounts',
