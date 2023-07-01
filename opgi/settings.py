5ed55c510f57eca0628576d3565be4f72d39e2dc
MohamedMeftouh21 <moaddameftouh@gmail.com>
2023-07-01 20:20:53 +0100

    first commit

--- opgi/settings.py
+++ opgi/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,6 +66,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
 ]
+
 CACHE_MIDDLEWARE_SECONDS = 0
 
 ROOT_URLCONF = 'opgi.urls'
