0729ea387ed28c2ab3086c9fa75438cc5c67d0e8
MohamedMeftouh21 <moaddameftouh@gmail.com>
2023-07-02 21:12:23 +0100

    first commit

--- recouvrement/routing.py
+++ recouvrement/routing.py
@@ -4,7 +4,8 @@ from . import consumers
 websocket_urlpatterns = [
     # Add more WebSocket URL patterns here
  path('ws/montant_mensuel/', consumers.MontantMensuelConsumer.as_asgi()),
-    path('ws/montant_mensuel_updates/<str:unit>/', consumers.MontantMensuelConsumer_by_unite.as_asgi()),
+re_path(r'^ws/montant_mensuel_updates/(?P<unit>\w+)/$', consumers.MontantMensuelConsumer_by_unite.as_asgi()),
+
     path('ws/montant_mensuel_updates_anne/<str:unit>/<int:anne>/', consumers.MontantMensuelConsumer_by_anne.as_asgi()),
     path('ws/chef_service/', consumers.ChefServiceConsumer.as_asgi()),
 ]
