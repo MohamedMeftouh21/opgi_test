1238bc861fe87512e190ce18bcc70124cf2fe2f6
MohamedMeftouh21 <moaddameftouh@gmail.com>
2023-07-02 02:09:17 +0100

    first commit

--- opgi/settings.py
+++ opgi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hgz-huuln209plc=v8v)ny4ve3=&1$j%#%ey=bx)3abmv1%7c#
 DEBUG = False
 
 ALLOWED_HOSTS = ['opgitest-production.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://opgitest-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://opgitest-production.up.railway.app']
 
 # Application definition
 
