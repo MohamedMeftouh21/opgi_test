1e017115b384e91721d166c12c575514ae5a3c43
MohamedMeftouh21 <moaddameftouh@gmail.com>
2023-07-03 01:02:23 +0100

    first commit

--- opgi/settings.py
+++ opgi/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hgz-huuln209plc=v8v)ny4ve3=&1$j%#%ey=bx)3abmv1%7c#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['opgitest-production.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://opgitest-production.up.railway.app']
+#ALLOWED_HOSTS = ['opgitest-production.up.railway.app', '127.0.0.1']
+#CSRF_TRUSTED_ORIGINS = ['https://opgitest-production.up.railway.app']
 
 # Application definition
 
