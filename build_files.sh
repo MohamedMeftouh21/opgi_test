1c521f0fd325cd820a7c74c821f3b388e948e867
MohamedMeftouh21 <moaddameftouh@gmail.com>
2023-07-03 17:31:44 +0100

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,12 @@
-# Update pip
-python3.9 -m pip install --upgrade pip
+#!/bin/bash
 
-# Install requirements
-pip install -r requirements.txt
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-# Collect Static Files on Deploy
-python3.9 manage.py collectstatic
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
