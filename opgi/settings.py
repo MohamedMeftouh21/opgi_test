fd2a3ba686b8d2f19ec30688f1d471abeeec1063
MohamedMeftouh21 <moaddameftouh@gmail.com>
2023-07-03 16:37:16 +0100

    first commit

--- opgi/settings.py
+++ opgi/settings.py
@@ -126,13 +126,14 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres',
+        'PASSWORD': 'Habiboutita1982gf',
+        'HOST': 'db.cimknvuzyvnilacxouvv.supabase.co', 
+        'PORT': '5432',
     }
 }
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600,ssl_require=True)
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
